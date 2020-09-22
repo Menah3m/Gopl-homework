62d77f3a0d87c118b4d1e5ece26a091141200675
Menah3m <568200065@qq.com>
2020-09-22 14:23:01 +0900

    add practice1.9

---
+++ ch1/practice1_9/main.go
@@ -0,0 +1,23 @@
+// This demo prints status code
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch:%v\n",err)
+		}
+
+		fmt.Printf("%v\n", resp.Status)
+	}
+}
